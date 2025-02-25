60443493d525b94cb613ab1130b8596501cbb1e8
Daniel De La Iglesia <diglesia@opentrends.net>
2025-02-25 11:09:52 +0100

    Changes from LoadEnv

--- app/core/lib/loadEnv.js
+++ app/core/lib/loadEnv.js
@@ -4,22 +4,49 @@ way.lib.loadEnv = async function (_args){
     setTimeout(() => {
       (async function() {
 
+        var fs = require('fs');
+        var path = require('path');
         var jsonToEnv = require('json-to-env2'); // https://github.com/nlapshin/json-to-env
 
         var envThis = await way.lib.decode({ data: way.env._this }).catch((o) => { return {} });
         const env = jsonToEnv(envThis, {});
-        var export_env = [];
+        //console.log(env)
+
+        var env_array = [];
         env.split(/\n/).forEach(env_var => {
           if (env_var === "") return;
           var a = env_var.split('=');
           if (/-/.test(a[0])) {
             env_var = env_var.replace(a[0], a[0].replace(/-/g,'_'));
           }
-          export_env.push(`export ${env_var}`);
+          if (a[1] !== "undefined" && a.length <= 2) {
+            env_array.push(`${env_var}`);
+          }
         });
-        way.var.env = `${export_env.join("; ")}`;
+        
+        // From env setting
+        if (way.lib.check(envThis['env']) && envThis['env'].length > 0) {
+          envThis['env'].forEach(env_var => {
+            var a = env_var.split('=');
+            if (a[1] !== "undefined" && a.length <= 2) {
+              env_array.push(`${env_var}`);
+            }
+          });
+        }
 
-        //console.log(way.var.env); way.lib.exit();
+        //console.log(env_array)
+        //env_array.forEach(env_var => {
+        //  console.log(env_var)
+        //});
+
+        way.var.loadEnv = env_array;
+
+        // way.var.env = `${env_array.join("; ")}`;
+        // console.log(way.var.env); 
+        // console.log('')
+        // way.var.env_dockerfile = `ENV ${env_array.join("\nENV ")}`;
+        // console.log(way.var.env_dockerfile); 
+
 
         return resolve({
           args: Object.assign({}, _args),
